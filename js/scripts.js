90fe1cc26b7400a5a9ac0d167ad51cb3538f3c21
Bwana <shirohigemw@gmail.com>
2017-10-17 10:21:57 +0300

    create all new folders

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//im just copy pasting at this pont - the likelyhood of understanding this, this week is 0. Best case scenario is hopefully by december.
+
+var add = function(number1, number2) {
+      return number1 + number2;
+    };
+
+ var add = function(number1, number2) {
+        return number1 + number2;
+      };
+
+     var number1 = parseInt (prompt("Enter a number:"));
+     var number2 = parseInt (prompt("Enter another number:"));
+
+      alert(add(number1, number2));
